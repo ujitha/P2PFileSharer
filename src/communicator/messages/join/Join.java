f83eff16705c81e28f6fc21796a5d727b92a3113
lasitha1991 <lasitha1991@hotmail.com>
2015-03-05 15:55:10 +0530

    communicator: message objects 2

---
+++ src/communicator/messages/join/Join.java
@@ -0,0 +1,33 @@
+package communicator.messages.join;
+
+import communicator.messages.Message;
+
+/**
+ * Created by lasitha on 3/5/15.
+ */
+public class Join extends Message{
+    /*
+     length JOIN IP_address port_no
+
+    e.g., 0027 JOIN 64.12.123.190 432
+    length – Length of the entire message including 4 characters used to indicate the length. In xxxx format.
+    JOIN – Join request.
+    IP_address – IP address in xxx.xxx.xxx.xxx format. This is the IP address other nodes will use to reach you. Indicated with up to 15 characters.
+    port_no – Port number. This is the port number that other nodes will connect to. Up to 5 characters.
+     */
+    private String ip;
+    private String port;
+
+    public Join(String ip,String port){
+        this.ip=ip;
+        this.port=port;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+}
---
+++ src/communicator/messages/register/AckUnregister.java
@@ -0,0 +1,28 @@
+package communicator.messages.register;
+
+import communicator.messages.Message;
+
+/**
+ * Created by lasitha on 3/5/15.
+ */
+public class AckUnregister extends Message {
+    /*
+    length UNROK value
+
+    e.g., 0012 UNROK 0
+    length – Length of the entire message including 4 characters used to indicate the length. In xxxx format.
+    UNROK – Unregister response.
+    value – Indicate success or failure.
+        0 – successful
+        9999 – error while unregistering. IP and port may not be in the registry or command is incorrect.
+    */
+    private int value;
+
+    public AckUnregister(int value){
+        this.value=value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
